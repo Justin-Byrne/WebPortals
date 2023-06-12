6bd9d3fbfb7d67941d6e1c88c3d1077e850b7565
Justin Don Byrne <justinbyrne001@gmail.com>
2023-06-12 14:44:47 -0700

    stuff

--- ClassGenerator/script/main.js
+++ ClassGenerator/script/main.js
@@ -1,5 +1,14 @@
 ////    GLOBALS     ////////////////////////////////////////
 
+    let contents =
+    {
+        'javascript-single':  'class ClassName\n{\n    _prop_one = 0;\n    _prop_two = \'string\';\n\n    constructor ( )         { /* code */ }\n\n    set prop_one ( value )  { /* code */ }\n    set prop_two ( value )  { /* code */ }\n\n    get prop_one ( )        { /* code */ }\n    get prop_two ( )        { /* code */ }\n\n    get method_get_only ( ) { /* code */ }\n\n    _private_method ( )     { /* code */ }\n\n    public_method ( )       { /* code */ }\n}',
+        'javascript-linked':  '/**\n * @class           {Object}      One                           Description\n * @property        {number}      prop0                         Description\n * @property        {string}      prop1                         Description\n * @property        {Two}         prop2                         Description\n * @property        {Three}       prop3                         Description\n */\nclass One\n{\n    _prop0 = 0;\n    _prop1 = \'string\';\n    _prop2 = new Two;\n    _prop3 = new Three;\n\n    constructor ( ) { }\n\n    set prop0 ( value ) { }\n    get prop0 ( ) { }\n\n    set prop1 ( value ) { }\n    get prop1 ( ) { }\n\n    set prop2 ( value ) { }\n    get prop2 ( ) { }\n\n    set prop3 ( value ) { }\n    get prop3 ( ) { }\n}',
+        'plantscript-single': '@startuml\n\nclass ClassName {\n    _prop_one   <color:gray>{number}</color>\n    _prop_two   <color:gray>{string}</color>\n    __ Setter __\n    prop_one\n    prop_two\n    __ Getter __\n    prop_one\n    prop_two\n    method_get_only\n    _private_method\n    public_method\n}\n@enduml',
+        'plantuml-single':    '<img src="images/uml_class.png">',
+        'plantuml-linked':    '<img src="images/linked-uml-class.png" class="small-image">'
+    }
+
     let _defaults =
     {
         stringsElement:         null,
@@ -37,9 +46,20 @@
 
     ////    SHORT HAND  ////////////////////////////
 
-        let setDefaults = ( typedObject ) => { for ( let _default in _defaults ) typedObject [ _default ] = _defaults [ _default ]; }
+        let setTypedDefaults = ( typedObject ) => { for ( let _default in _defaults ) typedObject [ _default ] = _defaults [ _default ]; }
 
     ////    COMMON  ////////////////////////////////
+
+        function injectContent ( )
+        {
+            for ( let content in contents )
+            {
+                let elements = document.getElementsByClassName ( content );
+
+
+                for ( let element of elements ) element.innerHTML = contents [ content ];
+            }
+        }
 
         function animationBackgroundFlicker ( elements )
         {
@@ -72,21 +92,19 @@
             }
         }
 
-        function addElementToScrollListener ( typedObject )
+        function setTypedListener ( typedObject )
         {
             window.addEventListener ( 'scroll', ( ) =>
             {
                 let elementBox = document.getElementById ( typedObject.el.id ).getBoundingClientRect ( );
 
 
-                (  ( elementBox.bottom < window.innerHeight )  &&  ( elementBox.bottom > elementBox.height )  )
+                ( elementBox.top    < window.innerHeight ) ? typedObject.start ( ) : typedObject.reset ( );
 
-                    ? typedObject.start ( )
-
-                    : typedObject.reset ( );
+                ( elementBox.bottom < 0                  ) ? typedObject.reset ( ) : typedObject.start ( );
             });
 
-            typed.options.onComplete = function ( )
+            typedObject.options.onComplete = function ( )
             {
                 let element = document.getElementById ( typedObject.el.parentElement.parentElement.getAttribute ( 'for' ) );
 
@@ -101,13 +119,29 @@
 
 ////    INSTANTIATION   ////////////////////////////////////
 
-    let typed = new Typed ( '#js-to-plantuml',
+    let typedPlantScript = new Typed ( '#js-to-plantscript',
     {
-        strings: [ '`&rarr; app : `python3^300 BuildClass.py^300 ~/Programs/JavaScript/Classes/class.js^700 ` &crarr;\n>>  [ output ]\n /Users/justinbyrne/Desktop/output/class.txt`' ],
+        strings: [ '`&rarr; app : `python3^300 BuildClass.py^300 ~/Programs/JavaScript/Classes/class.js^700 ` &crarr;\n>> [ output ]\n /Users/username/Programs/JavaScript/Classes/output/class.txt`' ],
+    } );
+
+    let typedPlantUml    = new Typed ( '#js-to-plantuml',
+    {
+        strings: [ '`&rarr; app : `python3^300 BuildClass.py^300 ~/Programs/JavaScript/Classes/class.js^300 -m^300 "png"^700 ` &crarr;\n>> [ output ]\n /Users/username/Programs/JavaScript/Classes/output/class.txt\n /Users/username/Programs/JavaScript/Classes/output/images/class.png`' ],
+    } );
+
+    let typedPlantLinkedUml = new Typed ( '#js-to-linked-plantuml',
+    {
+        strings: [ '`&rarr; app : `python3^300 BuildClass.py^300 ~/Programs/JavaScript/Classes/^300 -l^300 -m^300 "png"^700 ` &crarr;\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/two.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/two.png\n\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/one.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/one.png\n\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/three.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/three.png\n\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/two-linked.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/two-linked.png\n\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/three-linked.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/three-linked.png\n\n>> [ output ]\n /Users/username/Programs/Programs/JavaScript/Classes/output/one-linked.txt\n /Users/username/Programs/Programs/JavaScript/Classes/output/images/one-linked.png`' ],
     } );
 
 ////    POST    ////////////////////////////////////////////
 
-    setDefaults ( typed );
+    injectContent    ( );
 
-    addElementToScrollListener ( typed );
+    setTypedDefaults ( typedPlantScript    );
+    setTypedDefaults ( typedPlantUml       );
+    setTypedDefaults ( typedPlantLinkedUml );
+
+    setTypedListener ( typedPlantScript    );
+    setTypedListener ( typedPlantUml       );
+    setTypedListener ( typedPlantLinkedUml );
